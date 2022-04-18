f8a554b94591922ffaf54840fa7d6683e863573a
shaineo <shaineoneal@gmail.com>
2022-04-18 14:18:42 -0400

    idk

--- accum.h
+++ accum.h
@@ -2,17 +2,19 @@
 #define ACCUM_H_
 
 #include <vector>
+#include <string>
 
 template <typename T>
 //sums all items passed into function
 T accum(vector <T> v)
 {
-    return vector.at(0) + vector.at(1) + vector.at(2) + vector.at(3);
+    return v.at(0) + v.at(1) + v.at(2) + v.at(3);
 }
 
 std::string accum(vector <string> v)
 {
-
+    return v.at(0) + ", " + v.at(1) + ", " 
+        + v.at(2) + ", " + v.at(3);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    //vector<
+    vector<auto> input;
     cout << "Enter four numbers: ";
     cin >> 
 }
