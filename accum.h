aadb77321811333f08d9ac952743fa68631fb538
shaineo <shaineoneal@gmail.com>
2022-04-18 15:20:52 -0400

    issue with ", " in accum.h

--- accum.h
+++ accum.h
@@ -3,20 +3,23 @@
 
 #include <vector>
 #include <string>
+#include <sstream>
 
 template <typename T>
 //sums all items passed into function
-T accum(vector <T> v)
+T accum(std::vector <T> v)
 {
     return v.at(0) + v.at(1) + v.at(2) + v.at(3);
 }
 
-std::string accum(vector <string> v)
+std::string accum(std::vector <std::string> v)
 {
-    return v.at(0) + ", " + v.at(1) + ", " 
-        + v.at(2) + ", " + v.at(3);
+    std::stringstream ss;
+
+    ss << v.at(0) << ", " << v.at(1) << ", " 
+        << v.at(2) << ", " << v.at(3);
+    
+    return ss.str();
 }
-
-
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,7 +5,30 @@ using namespace std;
 
 int main()
 {
-    vector<auto> input;
+    int input;
+    string inputStr;
+    vector<int> inputVect;
+    vector<string> inputStrVect;
     cout << "Enter four numbers: ";
-    cin >> 
+    for(int i = 0; i < 4; i++)
+    {
+        cin >> input;
+        inputVect.push_back(input);
+    }
+
+    cout << "The sum of the numbers is " << accum<int>(inputVect) << endl;
+
+    //ask for strings
+    cout << "Enter four strings: ";
+    for(int i = 0; i < 4; i++)
+    {
+        cin >> inputStr;
+        inputStrVect.push_back(inputStr);
+    }
+
+    cout << "The sum of the strings is " << accum<string>(inputStrVect) << endl;
+
+
+
+
 }
